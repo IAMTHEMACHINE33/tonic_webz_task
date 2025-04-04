06b2e258e2a61e207fd805bd1a0ef93adfdda83b
IAMTHEMACHINE <iamthemachine@gmail.com>
2025-04-04 18:54:37 +0545

    todo: list of things to do

---
+++ app.js
@@ -0,0 +1,17 @@
+// TODO: Connect To DB from env (pg)
+// TODO: Hit API webz.io (using SDK Or Nah)
+    // TODO: Builder For Query in webz (Make queries extendable)
+        // wildcard asterisk *,
+        // disable stemming $, 
+        // encapsulate parenthesis (), AND +, OR , NOT -, 
+        // literal quotation "",
+// TODO: Query To Add every 200
+// TODO: Invoke callback with count & totalCount(totalResults)
+// TODO : unit test Cases (jest) (no calls to internet or db)
+// TODO : Docker compose
+
+// Ref
+// https://docs.webz.io/reference/news-api-lite
+// https://docs.webz.io/reference/cheat-sheet
+// https://github.com/Webhose/webzio-nodejs
+// https://api.webz.io/newsApiLite?token=[token]&q=Bitcoin
